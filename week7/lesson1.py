9abbc9bb99303b0f21413f1f873f315dff018d46
Sulaimanovuran <sulaimanovuran@gmail.com>
2022-06-21 20:04:05 +0600

    21/06/22

--- week7/lesson1.py
+++ week7/lesson1.py
@@ -143,3 +143,252 @@
 
 # o = Person('John', 23)
 # print(o.date(o.age))
+
+# class Square:
+#   def __init__(self, a, h):
+#     self.a = a
+#     self.h = h
+
+#   def get_s(self):
+#     return self.a * self.h
+
+
+# class Triangle:
+#   def __init__(self, b):
+#     self.b = b
+
+#   def get_ss(self, h):
+#     return 0.5 * self.b * h
+
+
+# class Pyramid(Square, Triangle):
+#   def __init__(self, a,h):
+#     Square.__init__(self, a,h)
+
+#   def get_volume(self, b):
+#     v = 1/3 * (self.get_s()) * b
+#     return round(v)
+
+
+
+# obj = Pyramid(5, 10)
+# print(obj.get_volume(6))
+
+
+#1/3 x основание^2 x высоту
+
+
+
+# class Coder:
+#   experience = 0
+#   count_code_time = 0
+
+#   def get_info(self):
+#     return None
+
+#   def coding(self, v):
+#     return None
+
+
+
+# class Backend(Coder):
+#   languages_backend = 'Python'
+
+#   def coding(self, v):
+#     self.count_code_time += v
+#     return self.count_code_time
+
+#   def get_info(self):
+#     return self.languages_backend, self.count_code_time
+
+
+# class Frontend(Coder):
+#   languages_frontend = 'JS'
+
+#   def coding(self, v):
+#     self.count_code_time += v
+#     return self.count_code_time
+
+#   def get_info(self):
+#     return self.languages_frontend, self.count_code_time
+
+
+# class FullStack(Frontend, Backend):
+#   def coding(self, v):
+#     self.count_code_time += v
+#     return self.count_code_time
+
+#   def get_info(self):
+#     return self.languages_frontend,self.languages_backend, self.count_code_time
+
+
+# dev_py = Backend()
+# dev_js = Frontend()
+# dev_fs = FullStack()
+
+# dev_py.coding(13)
+# dev_py.coding(13)
+# print(dev_py.get_info())
+# dev_js.coding(8)
+# print(dev_js.get_info())
+# dev_fs.coding(12)
+# print(dev_fs.get_info())
+
+
+
+
+# import time
+# class Clock:
+#   def get_time(self):
+#     time_str = time.strftime('%H:%M:%S')
+#     return time_str
+    
+                        
+
+# class Alarm:
+#   def on(self):
+#     print('Alarm turn on')
+#   def off(self):
+#     print('Alarm turn off')
+
+# class AlarmClock(Clock, Alarm):
+#   def alarm(self):
+#     return self.on()
+
+# obj = AlarmClock()
+# obj.alarm()
+# print(obj.get_time())
+# import math
+# class AreaMixin:
+#   def area(self,a=0,b=0,c=0,):
+#     self.name = input('Введите название фигуры: ')
+#     if self.name == 'Куб':
+#       return 6 * a**2
+#     elif self.name == 'Пирамида':
+#       return round(((a**2)*math.sqrt(3)/4)+((3*a/2)*math.sqrt(b *2+a**2/12)))
+#     elif self.name == 'Шар':
+#       return 4 * math.pi * (a**2)
+#     elif self.name == 'Цилиндр': #2 π R h + 2 π R 2
+#       return (2 * math.pi * a * b) + (2 * math.pi * a**2)
+#     elif self.name == 'Параллелепипед':  # S = 2(a · b + a · h + b · h)
+#       return 2 * (a * b + a * c + b * c)
+
+
+# class Square(AreaMixin):
+#   def __init__(self, a, h):
+#     self.a = a
+#     self.h = h
+
+#   def get_s(self):
+#     return self.a * self.h
+
+
+# class Triangle:
+#   def __init__(self, b):
+#     self.b = b
+
+#   def get_ss(self, h):
+#     return 0.5 * self.b * h
+
+
+# class Pyramid(Square, Triangle):
+#   def __init__(self, a,h):
+#     Square.__init__(self, a,h)
+
+
+# class Cube(Square):
+#   def hello(self):
+#     print('hello im a cube')
+
+
+
+# c = Cube(4,5)
+# print(c.area(4))
+
+# p = Pyramid(4,5)
+# print(p.area(4,5))
+
+# class CreateMixin:
+#   def create(self,dict, k, v):
+#     dict[k] = v
+#     return dict
+
+# class DeleteMixin:
+#   def delete(self, dict, k,):
+#     x = dict.pop(k)
+#     return f'Удалили задачу: {x}'
+
+# class UpdateMixin:
+#   def update(self, dict, v):
+#     return dict.update(v)
+
+# class ReadMixin:
+#   def sort(self, dict_):
+#     sorted_d = sorted(dict_.items(), key =lambda x: x[0])
+#     return dict(sorted_d)
+
+  
+# class ToDo(CreateMixin, DeleteMixin, UpdateMixin, ReadMixin):
+#   todos = {}
+#   def __init__(self):
+#     pass
+#   def set_deadline(self, date):
+#     import time
+#     x = time.localtime(time.time())
+#     res = date - int(x.tm_mday)
+#     return res
+
+# o = ToDo()
+
+# o.create(o.todos, 1, "Уборка")
+# o.create(o.todos, 4, "Учеба")
+# o.create(o.todos, 2, "Работа")
+# o.create(o.todos, 3, "Прогулка")
+
+# print(o.todos)
+
+# print(o.delete(o.todos, 1))
+# print(o.todos)
+
+# o.update(o.todos,{2:'Тренировка'})
+# print(o.todos)
+
+# print(o.sort(o.todos))
+
+# class ExtensionMixin:
+#   def add_extension(self, ls, el, nm):
+#     ls.append(el)
+#     print(f'Добавлено новое расширение {el} для игры {nm}')
+
+#   def remove_extension(self, ls, el, nm):
+#     ls.remove(el)
+#     print(f'{el} был отключен от {nm}')
+
+
+# class Game(ExtensionMixin):
+#   def __init__(self,game_name,game_type):
+#     self.game_type = game_type
+#     self.game_name = game_name
+#     self.extensions = []
+
+#   def get_description(self):
+#     print(f'{self.game_name}-это {self.game_type}')
+
+#   def get_extensions(self):
+#     if self.extensions == []:
+#       print('No')
+#     else:
+#       print(self.extensions)
+
+
+# o = Game('CS', 'шутер от первого лица, в  режиме командного боя')
+# o.add_extension(o.extensions, 'gravity', o.game_name)
+# o.get_description()
+# o.remove_extension(o.extensions, 'gravity', 'CS')
+# print(o.extensions)
+
+
+# x = input()
+# list_ = x.split(' ')
+# res = list_.sort(reverse = True)
+# print(list_[1])
---
+++ week7/lesson2.py
@@ -0,0 +1,175 @@
+"""
+Полиморфизм - возможность использовать один и тот же метод для 
+разных классов, при этом результат может меняться в зависимости от того
+к какому классу принадлежит обьект
+
+
+Полиморфизм - множество форм
+"""
+
+# def add(x,y):
+#     return x + y
+    
+# print(add(5,5))
+# print(add('5', '5'))
+
+
+# print(len('python'))
+# print(len([1,2,3,4,5]))
+# print(len({'a':8}))
+
+
+# class Cat:
+#     def __init__(self,name, age):
+#         self.name = name
+#         self.age = age
+#     def make_sound(self):
+#         return 'Meoww'
+
+
+# class Dog:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+#     def make_sound(self):
+#         return 'Gav'
+
+# cat = Cat('Barsik', 10)
+# dog = Dog('Rex', 8)
+
+# print(cat.make_sound())
+# print(dog.make_sound())
+
+# list_ = [cat, dog]
+
+# for i in list_:
+#     print(i.make_sound())
+
+
+# from math import pi
+
+# class ShapeMixin:
+#     '''Класс для описания форм'''
+#     def __init__(self, name):
+#         self.name = name
+#     def __str__(self) -> str:
+#         return self.name
+
+# class Square(ShapeMixin):
+#     def __init__(self, lenght):
+#         super().__init__('Square') 
+#         self.lenght = lenght
+
+#     def area(self):
+#         return self.lenght ** 2
+
+# class Circle(ShapeMixin):
+#     def __init__(self, radius):
+#         super().__init__('Circle')
+#         self.radius = radius
+
+#     def area(self):
+#         return self.radius **2 * pi
+
+
+
+# square1 = Square(4)
+# square2 = Square(2)
+
+# circle1 = Circle(10)
+# circle2 = Circle(5)
+
+# list_ = [square1, square2, circle1, circle2]
+# for i in list_:
+#     print(i.area())
+
+# [print(i.area()) for i in list_]
+
+
+'''
+Абстракция - абстрактный класс опредеялет общий интерфейс для набора подклассов
+Заставляя дочерние классы рефлизовывать абстрактные методы
+'''
+
+
+# from abc import ABC, abstractclassmethod
+# class A(ABC):
+#     def method1(self):
+#         pass
+#     @abstractclassmethod
+#     def method2(self):
+#         pass
+
+# class B(A):
+#     def method2(self):
+#         pass
+
+# b = B()
+
+# from abc import ABC, abstractclassmethod
+# class Peaople(ABC):
+#     @abstractclassmethod
+#     def breathe(self):
+#         pass
+#     @abstractclassmethod
+#     def eat(self):
+#         pass
+#     @abstractclassmethod
+#     def move(self):
+#         pass
+#     @abstractclassmethod
+#     def sleep(self):
+#         pass
+
+
+# class Person(Peaople):
+#     def breathe(self):
+#         print('я умею дышать')
+#     def sleep(self):
+#         print('я умею дышать')
+#     def eat(self):
+#         print('я умею есть')
+#     def move(self):
+#         print('я умею ходить')
+
+# obj = Person()
+
+
+# class Animals(ABC):
+#     @abstractclassmethod
+#     def method1(self):
+#         pass
+#     def method2(self):
+#         print('я')
+
+
+# class Cat(Animals):
+#     def __init__(self, name):
+#         self.name= name
+
+#     def method1(self):
+#         print(f'im a cat')
+
+#     def method2(self):
+#         print(f'my name is {self.name}')
+
+# class Dog(Animals):
+#     def __init__(self, name):
+#         self.name = name
+
+#     def method1(self):
+#         print('im a dog')
+
+#     def method2(self):
+#         print(f'my name is {self.name}')
+
+# list_ = range(1,100)
+# for i in list_:
+#     if i % 3 == 0 and i % 5 == 0:
+#         print('fizzbuzz')
+#     elif i % 3 == 0:
+#         print('fizz')
+#     elif i % 5 == 0:
+#         print('buzz')
+
